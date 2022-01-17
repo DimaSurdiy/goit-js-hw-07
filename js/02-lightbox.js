b82c898e87ca082c53dfe8608a7d378ae3f8a32f
DimaSurdiy <88757113+DimaSurdiy@users.noreply.github.com>
2022-01-17 21:57:54 +0200

    adds completed task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+/** 
+1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. Используй готовый код из первого задания. 
+*/
+
+const galleryEl = document.querySelector('.gallery');
+
+createGalleryItemsMarkup();
+
+galleryEl.addEventListener('click', onGalleryItemsClick);
+
+function createGalleryItemsMarkup() {
+  const galleryItemsMarkup = galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+    </a>
+    `;
+    })
+    .join('');
+
+  galleryEl.innerHTML = galleryItemsMarkup;
+}
+
+function onGalleryItemsClick(evt) {
+  evt.preventDefault();
+
+  var lightbox = new SimpleLightbox('.gallery a');
+}
