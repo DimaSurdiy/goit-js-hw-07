ae9c29a87572ba24ee60a1ac73d7fa934043a1c7
DimaSurdiy <dsurdiy@ukr.net>
2022-07-15 07:39:40 +0300

    gallery.js code refactoring

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,52 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 
-const galleryEl = document.querySelector('.gallery');
+const galleryContainerEl = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
 
-createGalleryItemsMarkup();
+galleryContainerEl.insertAdjacentHTML('beforeend', galleryItemsMarkup);
 
-galleryEl.addEventListener('click', onGalleryItemsClick);
+galleryContainerEl.addEventListener('click', onGalleryClick);
 
-function createGalleryItemsMarkup() {
-  const galleryItemsMarkup = galleryItems
-    .map(({ original, preview, description }) => {
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
       return `
         <div class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-                />
-            </a>
+          <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          /></a>
         </div>
-    `;
+        `;
     })
     .join('');
-
-  galleryEl.innerHTML = galleryItemsMarkup;
 }
 
-function onGalleryItemsClick(evt) {
+function onGalleryClick(evt) {
   evt.preventDefault();
 
-  if (evt.currentTarget === evt.target) {
+  if (!evt.target.classList.contains('gallery__image')) {
     return;
   }
 
-  const galleryItem = evt.target;
+  const originalImage = evt.target.dataset.source;
+  const lightbox = basicLightbox.create(`<img src="${originalImage}">`);
 
-  const galleryItemModal = basicLightbox.create(`
-      <img src="${galleryItem.dataset.source}" alt="${galleryItem.alt}"/>
-    `);
-  galleryItemModal.show();
+  showOriginalGalleryImage(lightbox);
 
-  const isGalleryItemModalVisible = galleryItemModal.visible();
-  if (isGalleryItemModalVisible) {
-    document.addEventListener('keydown', evt => {
-      if (evt.code === 'Escape') {
-        galleryItemModal.close();
-      }
-    });
+  window.addEventListener('keydown', onEscapeKeydown);
+
+  function onEscapeKeydown(e) {
+    if (e.code === 'Escape') {
+      lightbox.close();
+      window.removeEventListener('keydown', onEscapeKeydown);
+    }
   }
+}
+
+function showOriginalGalleryImage(lightbox) {
+  lightbox.show();
 }
